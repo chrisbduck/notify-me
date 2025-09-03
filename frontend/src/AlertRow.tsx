e916a9f24b888389e240c8740c5ccab6abef3545
Chris Bevan <>
2025-09-02 22:10:33 -0700

    fixed possibly-undefined errors in AlertRow

--- frontend/src/AlertRow.tsx
+++ frontend/src/AlertRow.tsx
@@ -3,20 +3,19 @@ import './AlertRow.css';
 import { FaExclamationTriangle, FaInfoCircle, FaTimesCircle, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { Severity, type AlertModel } from './model';
 
-const renderSeverityIcon = (severity: Severity) => {
+const renderSeverityIcon = (severity?: Severity) => {
   switch (severity) {
     case Severity.WARNING:
       return <FaExclamationTriangle className="alert-icon warning-icon" />;
     case Severity.INFO:
+    default:
       return <FaInfoCircle className="alert-icon info-icon" />;
     case Severity.SEVERE:
       return <FaTimesCircle className="alert-icon severe-icon" />;
-    default:
-      return null;
   }
 };
 
-const getSeverityClass = (severity: Severity) => {
+const getSeverityClass = (severity?: Severity) => {
   switch (severity) {
     case Severity.WARNING:
       return 'severity-warning';
@@ -46,11 +45,11 @@ const AlertRow: React.FC<{ alert: AlertModel }> = ({ alert }) => {
       </div>
       <>
         <div className="alert-description-row">
-          <p>{alert.description_text.translation[0]?.text}</p>
+          <p>{alert.description_text?.translation[0]?.text}</p>
         </div>
         <div className="alert-footer-row">
-          <p><strong>Effect:</strong> {alert.effect} ({alert.effect_detail.translation[0]?.text})</p>
-          <p><strong>Cause:</strong> {alert.cause} ({alert.cause_detail.translation[0]?.text})</p>
+          <p><strong>Effect:</strong> {alert.effect} ({alert.effect_detail?.translation[0]?.text})</p>
+          <p><strong>Cause:</strong> {alert.cause} ({alert.cause_detail?.translation[0]?.text})</p>
           <p><strong>Severity:</strong> {alert.severity_level}</p>
           {alert.url?.translation[0]?.text && (
             <p>
